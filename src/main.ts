6c36e0086bf1eebfa8f3e9981448c85fc937cf54
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-11-15 23:00:42 +0200

    fix(main): making the Application  running on port 8000 resolve the TCP health check failures

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(8000);
+  console.log(`Application is running on port 8000`);
 }
 bootstrap();
