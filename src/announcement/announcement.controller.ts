de80b80891105a5e3656a6629c51a6cbd28055e7
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-11-15 10:35:02 +0200

    feat(guards): adding the authorization and auth guards and user module and protecting all routes with the guards by their roles.

--- src/announcement/announcement.controller.ts
+++ src/announcement/announcement.controller.ts
@@ -6,16 +6,22 @@ import {
   Param,
   Put,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AnnouncementService } from './announcement.service';
 import { CreateAnnouncementDto } from './dto/create-announcement.dto';
 import { MongoIdPipe } from '../common/pipes/mongo-id.pipe';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('announcements')
 export class AnnouncementController {
   constructor(private readonly announcementService: AnnouncementService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'teacher')
   create(@Body() createAnnouncementDto: CreateAnnouncementDto) {
     return this.announcementService.create(createAnnouncementDto);
   }
@@ -31,6 +37,8 @@ export class AnnouncementController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'teacher')
   update(
     @Param('id', MongoIdPipe) id: string,
     @Body() updateAnnouncementDto: CreateAnnouncementDto,
@@ -39,6 +47,8 @@ export class AnnouncementController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'teacher')
   remove(@Param('id', MongoIdPipe) id: string) {
     return this.announcementService.remove(id);
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuizModule } from './quiz/quiz.module';
 import { AnnouncementModule } from './announcement/announcement.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -13,6 +15,8 @@ import { AnnouncementModule } from './announcement/announcement.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     QuizModule,
     AnnouncementModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
