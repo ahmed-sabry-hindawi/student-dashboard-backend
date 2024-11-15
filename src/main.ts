59f34a363c1cee67098a9fe6c30487d4b99cc133
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-11-15 18:21:30 +0200

    feat(vercel.json):adding some configration for deploying into vercel.
    fix(main.ts): Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +15,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
