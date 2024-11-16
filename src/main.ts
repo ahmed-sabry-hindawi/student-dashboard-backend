f0c87b55c7493194a8c133b7cc90ca3b3216074e
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-11-16 08:40:25 +0200

    test for production

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000;
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -19,7 +19,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(8000);
-  console.log(`Application is running on port 8000`);
+  await app.listen(port);
+  console.log(`Environment Port: ${process.env.PORT}`);
 }
 bootstrap();
